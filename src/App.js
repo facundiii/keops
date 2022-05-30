353b080ffd1a6805d1c05826618649e14036efc7
facundiii <facundofernandezgoyechea@iresm.edu.ar>
2022-05-29 22:47:26 -0300

    Cambios 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
-import Navbar from './components/Navbar';
+import {BrowserRouter as Router} from 'react-router-dom';
+import Home from './pages';
 
 function App() {
   return (
-    <Navbar />
+    <Router>
+      <Home />
+    </Router>
+
 
   );
 }
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,9 +1,34 @@
 import React from 'react';
-import {Nav} from './NavbarElements'
+import {FaBars} from 'react-icons/fa'
+import { Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks, NavBtn, NavBtnLink } from './NavbarElements'
 
-const Navbar = () => {
+
+const Navbar = ({toggle}) => {
     return (
         <Nav>
+            <NavbarContainer>
+                <NavLogo to='/'>SSS</NavLogo>
+                <MobileIcon onClick={toggle}>
+                    <FaBars />
+                </MobileIcon>
+                <NavMenu>
+                    <NavItem>
+                        <NavLinks to='about'>About</NavLinks>
+                    </NavItem>
+                    <NavItem>
+                        <NavLinks to='about'>About</NavLinks>
+                    </NavItem>
+                    <NavItem>
+                        <NavLinks to='about'>About</NavLinks>
+                    </NavItem>
+                    <NavItem>
+                        <NavLinks to='about'>About</NavLinks>
+                    </NavItem>
+                </NavMenu>
+                <NavBtn>
+                    <NavBtnLink to='/signin'>Compra</NavBtnLink>
+                </NavBtn>
+            </NavbarContainer>
 
         </Nav>
     )
